b43e9fe77ed5de076e767263a97aa395efd35118
Awet Tesfay <whitezscope@gmail.com>
2020-12-27 22:43:14 +0100

    create laminas app, configure it and link it to data base and read from database

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Album\Controller;
+
+
+use Album\Model\AlbumTable;
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    private $table;
+
+    // Add this constructor:
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+
+    public function indexAction(): ViewModel
+    {
+        return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
+    }
+}
---
+++ module/Album/src/Model/AlbumTable.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Album\Model;
+
+
+use Laminas\Db\TableGateway\TableGatewayInterface;
+
+class AlbumTable
+{
+    private $tableGateway;
+
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function fetchAll()
+    {
+        return $this->tableGateway->select();
+    }
+
+}
